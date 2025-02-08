4b57ccb7bf548b2c49198edca5c68671215d6a28
gxz <cnmmail163@163.com>
2025-02-08 10:30:44 +0800

    修复SSR

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,7 +1,13 @@
 import {Dispatch, SetStateAction, useEffect, useState} from "react";
 
 export default function useLocalStorage(key: string): [string, Dispatch<SetStateAction<string>>] {
-    const [value, setValue] = useState(typeof window === 'undefined' ? '' : window.localStorage.getItem(key) ?? '')
+    const [value, setValue] = useState('')
+    useEffect(() => {
+        if (typeof window === 'undefined') return
+        const newValue = window.localStorage.getItem(key)
+        if (newValue === null) return
+        setValue(newValue)
+    }, [])
     useEffect(() => {
         if (typeof window === 'undefined') return
         if (value === '') window.localStorage.removeItem(key)
